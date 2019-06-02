ed4fdbf180c3ebf9fc2639c49d0564534cb8d9d4
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-06-02 13:15:07 -0700

    have two different containers. 1 for tests and 1 for the actual app

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>HELLO MACK</h1>
+        <h1>GOODBYE MACK</h1>
+        <h1>GOODBYE MACK 2.0</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
